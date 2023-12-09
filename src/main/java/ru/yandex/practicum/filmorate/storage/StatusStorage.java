6116372eaea9800455318c438ad9e33e3f536de9
artOops <sir.tema2014@yandex.ru>
2023-12-10 00:14:07 +0500

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/StatusStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/StatusStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.user.Status;
+
 @Deprecated
 public interface StatusStorage extends AbstractStorage<Status> {
 }
